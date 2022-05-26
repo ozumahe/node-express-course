cbcb9d53e831c706eefc079726887693e00cf762
ozumahe <ozumahe@gmail.com>
2022-05-26 21:47:44 +0100

    update

--- 10-e-commerce-api/starter/controllers/UserController.js
+++ 10-e-commerce-api/starter/controllers/UserController.js
@@ -28,7 +28,28 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-  res.send(req.body);
+  const { old_password, new_password } = req.body;
+
+  if (!old_password || !new_password) {
+    throw new Error.BadRequestError(
+      "Please Old password and New password are required"
+    );
+  }
+  const { userId } = req.user;
+
+  const user = await UserSchema.findOne({ _id: userId });
+
+  const isPasswordCorrect = await user.comparePassword(old_password);
+
+  if (!isPasswordCorrect) {
+    throw new Error.UnauthenticatedError("Invalid Credentials");
+  }
+
+  user.password = new_password;
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Sucess! Password Updated" });
 };
 
 module.exports = {
