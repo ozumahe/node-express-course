5dddf6c479bac9c3500026c494dfc069339617d1
ozumahe <ozumahe@gmail.com>
2022-06-07 08:46:22 +0100

    update

---
+++ 10-e-commerce-api/starter/models/OrderSchema.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const SingleCartItem = mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
+const OrderSchema = mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [SingleCartItem],
+    status: {
+      type: String,
+      enum: ["pending", "failed", "paid", "delivered", "canceled"],
+      default: "pending",
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clientSecret: {
+      type: Number,
+      required: true,
+    },
+    paymentIntentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Orders", OrderSchema);
