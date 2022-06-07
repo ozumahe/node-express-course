162afb05b55513a5b87d4067aeb2549eded74a0b
ozumahe <ozumahe@gmail.com>
2022-06-07 17:57:09 +0100

    update

--- 10-e-commerce-api/starter/controllers/ordersController.js
+++ 10-e-commerce-api/starter/controllers/ordersController.js
@@ -1,5 +1,9 @@
 const OrderSchema = require("../models/OrderSchema");
 
+const createOrder = async (req, res) => {
+  res.send("create Order");
+};
+
 const getAllOrders = async (req, res) => {
   res.send("Get All Orders");
 };
@@ -8,9 +12,6 @@ const getSingleOrder = async (req, res) => {
 };
 const getCurrentUserOrder = async (req, res) => {
   res.send("Get Current User Order");
-};
-const createOrder = async (req, res) => {
-  res.send("create Order");
 };
 const updateOrder = async (req, res) => {
   res.send("update Order");
