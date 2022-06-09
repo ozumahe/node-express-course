a07c3e3c937b213a0b797802252194e562738df8
ozumahe <ozumahe@gmail.com>
2022-06-09 14:18:47 +0100

    update

--- 10-e-commerce-api/starter/models/OrderSchema.js
+++ 10-e-commerce-api/starter/models/OrderSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const SingleCartItem = mongoose.Schema({
+const SingleOrderItem = mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
@@ -30,7 +30,7 @@ const OrderSchema = mongoose.Schema(
       type: Number,
       required: true,
     },
-    cartItems: [SingleCartItem],
+    orderItems: [SingleOrderItem],
     status: {
       type: String,
       enum: ["pending", "failed", "paid", "delivered", "canceled"],
