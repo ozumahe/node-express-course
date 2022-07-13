3b672fa75a42061f0b4f632718d91f90b3b02c47
ozumahe <ozumahe@gmail.com>
2022-07-13 06:23:30 +0100

    update

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -40,10 +40,26 @@ const register = async (req, res) => {
 };
 
 // VERIFY EMAIL
-const verifyEmail = (req, res) => {
+const verifyEmail = async (req, res) => {
   const { verificationToken, email } = req.body;
 
-  res.status(StatusCodes.OK).json({ verificationToken, email });
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
 };
 
 // LOGIN
